4c94039104607a48081c8b281d39aa64e184bddb
tswsxk <tongsw@mail.ustc.edu.cn>
2020-01-07 16:23:26 +0800

    coverage fix

--- tests/test_utils/test_embeddings.py
+++ tests/test_utils/test_embeddings.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 # 2020/1/4 @ tongshiwei
 
+import pytest
 from gluonnlp.embedding import TokenEmbedding
 from CangJie.utils.embeddings import load_embedding, token_to_idx, get_embedding_size, get_embedding_array
 
@@ -12,6 +13,9 @@ def test_load_embeddings(vec_csv, tmpdir):
 
     assert isinstance(embeddings["a"], TokenEmbedding)
     assert isinstance(embeddings["b"], TokenEmbedding)
+
+    with pytest.raises(TypeError):
+        token_to_idx(embeddings["a"], [123, 456])
 
     token_to_idx(embeddings["a"], ["仓颉", "龙"])
 
